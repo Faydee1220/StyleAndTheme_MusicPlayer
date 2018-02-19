b1ecbb376c99197f475283b9c13c71dc5c204c47
Faydee <faydee1220@gmail.com>
2018-02-19 19:55:05 +0800

    first commit

---
+++ app/src/main/java/com/teamtreehouse/stylesandthemes/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.teamtreehouse.stylesandthemes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.SwitchCompat;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SettingsActivity extends AppCompatActivity {
+
+  @BindView(R.id.toolbar) Toolbar toolbar;
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+    ButterKnife.bind(this);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
+  }
+}
